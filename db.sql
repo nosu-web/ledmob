937982c50569c2914416d2e59ab898ed5729f470
NOSU-WEB <79894331+nosu-web@users.noreply.github.com>
2022-05-18 09:56:18 +0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,40 +1,32 @@
 CREATE TABLE `room` (
 	`id` int NOT NULL AUTO_INCREMENT,
-	`name` varchar(100) NOT NULL AUTO_INCREMENT,
-	`code` varchar(100) NOT NULL AUTO_INCREMENT,
-	`author_id` varchar(100) NOT NULL AUTO_INCREMENT,
-	`scenario_id` int NOT NULL AUTO_INCREMENT,
-	`status` bool NOT NULL,
+	`name` varchar(100) NOT NULL,
+	`code` varchar(100) NOT NULL,
+	`author_id` varchar(100) NOT NULL,
+	`scenario_id` int NOT NULL,
+	`status` tinyint NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `author` (
 	`id` int NOT NULL AUTO_INCREMENT,
-	`login` varchar(50) NOT NULL AUTO_INCREMENT UNIQUE,
-	`password` varchar(100) NOT NULL AUTO_INCREMENT,
+	`login` varchar(50) NOT NULL UNIQUE,
+	`password` varchar(100) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `member` (
 	`id` int NOT NULL AUTO_INCREMENT,
-	`room_id` int NOT NULL AUTO_INCREMENT,
-	`device` varchar(200) NOT NULL AUTO_INCREMENT UNIQUE,
+	`room_id` int NOT NULL,
+	`device` varchar(200) NOT NULL UNIQUE,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `scenario` (
 	`id` int NOT NULL AUTO_INCREMENT,
-	`name` varchar(50) NOT NULL AUTO_INCREMENT,
+	`name` varchar(50) NOT NULL,
 	`description` varchar(250) NOT NULL,
 	PRIMARY KEY (`id`)
 );
-
-ALTER TABLE `room` ADD CONSTRAINT `room_fk0` FOREIGN KEY (`author_id`) REFERENCES `author`(`id`);
-
-ALTER TABLE `room` ADD CONSTRAINT `room_fk1` FOREIGN KEY (`scenario_id`) REFERENCES `scenario`(`id`);
-
-ALTER TABLE `member` ADD CONSTRAINT `member_fk0` FOREIGN KEY (`room_id`) REFERENCES `room`(`id`);
-
-
 
 
